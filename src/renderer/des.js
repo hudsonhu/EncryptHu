4bb2f8579775b29f5a991c0191edd8d289d6df5e
TIansu Hu <tiansu.hu@ucdconnect.ie>
2023-10-05 12:38:59 +0800

    seems like core func correct

--- src/renderer/des.js
+++ src/renderer/des.js
@@ -34,18 +34,22 @@ function initialPermutation(plaintext) {
     return permute(plaintext, IP);
 }
 
-function iterate_16(plaintext, keys) {
+function iterate_16(plaintext, keys, encrypt = true) {
     let L = plaintext.slice(0, 32);
     let R = plaintext.slice(32, 64);
+
+    if (!encrypt) {
+        keys = keys.reverse();
+    }
 
     for (let i = 0; i < 16; i++) {
         let newR = xor(L, f(R, keys[i]));
         L = R;
         R = newR;
     }
-
-    return R.concat(L); // R+L
+    return R.concat(L);
 }
+
 
 
 function generateKey(key) {
@@ -85,7 +89,8 @@ function xor(block1, block2) {
 
 function f(block, key) {
     let expandedBlock = permute(block, E);  // expand to 48 bits
-    let temp = xor(expandedBlock, key);
+    // console.log("expandedBlock: ", expandedBlock);
+    let temp = xor(expandedBlock, key);  // E(R) xor K
     let sBoxOutput = [];
 
     // split into 8 groups of 6 bits
@@ -98,8 +103,10 @@ function f(block, key) {
         let bin = val.toString(2).padStart(4, '0').split('').map(Number);
         sBoxOutput = sBoxOutput.concat(bin);
     }
-
-    return permute(sBoxOutput, P);
+    // console.log("sBoxOutput: ", sBoxOutput);
+    let result = permute(sBoxOutput, P);
+    // console.log("result: ", result);
+    return result;
 }
 
 /**
@@ -112,19 +119,25 @@ function desEncryptBlock(plaintext, subkeys) {
     // plaintext: binary string
     // subkeys: array of 16 48-bit keys
     plaintext = initialPermutation(plaintext);
+    // console.log(plaintext);
     let encryptedText = iterate_16(plaintext, subkeys);
     encryptedText = permute(encryptedText, IP_INV);
+    // console.log(encryptedText);
     return encryptedText;
 }
 
 
 /**
  * Encrypt plaintext with DES
- * @param {string} plaintext - binary string
- * @param {number[]} key - 64-bit binary array
+ *
+ * @param {string} plaintextHex - Hexadecimal string representing the plaintext.
+ * @param {string} keyHex - Hexadecimal string representing the 64-bit key.
  * @returns {*[]}
  */
-function desEncrypt(plaintext, key) {
+function desEncrypt(plaintextHex, keyHex) {
+    // Convert hex to binary
+    const plaintext = hexToBinary(plaintextHex);
+    const key = hexToBinary(keyHex).split('').map(bit => parseInt(bit, 10));  // Convert string bits to numbers
     let subkeys = generateSubkeys(key);
 
     // cut plaintext into 64-bit blocks
@@ -137,17 +150,83 @@ function desEncrypt(plaintext, key) {
     for (let i = 0; i < blocks.length; i++) {
         block = desEncryptBlock(blocks[i], subkeys);
         block = block.join('');
-
+        blocks[i] = block;
     }
+
+    // convert blocks to hex
+    blocks = blocks.map(binaryToHex);
+    let encrypted_result = blocks.join('');
+    // console.log(encrypted_result);
+    return encrypted_result;
 }
 
 
+function desDecryptBlock(ciphertext, subkeys) {
+    ciphertext = initialPermutation(ciphertext);
+    let decryptedText = iterate_16(ciphertext, subkeys, false);
+    decryptedText = permute(decryptedText, IP_INV);
+    return decryptedText;
+}
+
+function desDecrypt(ciphertextHex, keyHex) {
+    // Convert hex to binary
+    const ciphertext = hexToBinary(ciphertextHex);
+    const key = hexToBinary(keyHex).split('').map(bit => parseInt(bit, 10));  // Convert string bits to numbers
+    let subkeys = generateSubkeys(key);
+
+    // cut ciphertext into 64-bit blocks
+    let blocks = [];
+    for (let i = 0; i < ciphertext.length; i += 64) {
+        blocks.push(ciphertext.slice(i, i + 64));
+    }
+
+    // process each block
+    for (let i = 0; i < blocks.length; i++) {
+        block = desDecryptBlock(blocks[i], subkeys);
+        block = block.join('');
+        blocks[i] = block;
+    }
+
+    // convert blocks to hex
+    blocks = blocks.map(binaryToHex);
+    let decrypted_result = blocks.join('');
+    return decrypted_result;
+}
+
+/**
+ * Convert a hexadecimal string to its binary representation.
+ *
+ * @param {string} hex - hexadecimal string to be converted.
+ * @returns {string} binary representation of hex
+ */
 function hexToBinary(hex) {
     return hex.split('').map((char) => parseInt(char, 16).toString(2).padStart(4, '0')).join('');
 }
 
+function binaryToHex(binary) {
+    return binary.match(/.{1,4}/g).map((bin) => parseInt(bin, 2).toString(16)).join('');
+}
+
+function pkcs7Pad(data) {
+    const padLength = 8 - (data.length / 2) % 8;
+    const padByte = padLength.toString(16).padStart(2, '0');
+    const padding = padByte.repeat(padLength);
+    return data + padding;
+}
+
+function pkcs7Unpad(data) {
+    const lastByte = parseInt(data.slice(-2), 16);
+    return data.slice(0, -lastByte * 2);
+}
+
+
+// TODO: remove
+console.log(desEncrypt('0123456789ABCDEF', '133457799BBCDFF1'));
+// it should be 85E813540F0AB405
+
 
 module.exports = {
     desEncrypt,
-    generateRandomKeyWithParity,
+    pkcs7Pad,
+    pkcs7Unpad
 };
--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -1,39 +1,9 @@
 const { desEncrypt } = require('./des.js');
+const {pkcs7Pad} = require("./des");
 
-function textToBinary(text) {  // Transform text to binary representation(8-bit)
-    const textEncoder = new TextEncoder();   // go UTF-8
-    const byteArray = textEncoder.encode(text);
-
-    let binaryStr = '';
-    for (let byte of byteArray) {
-        binaryStr += byte.toString(2).padStart(8, '0');  // go Binary
-    }
-
-    return binaryStr;
+function utf8ToHex(str) {
+    return Array.from(str).map(c => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');
 }
-
-const text = "bad";
-const binaryRepresentation = textToBinary(text);
-console.log(binaryRepresentation);
-
-function binaryToText(binaryStr) {  // Transform binary representation(8-bit) to text
-    const byteCount = binaryStr.length / 8;
-    const byteArray = new Uint8Array(byteCount);
-
-    for (let i = 0; i < byteCount; i++) {
-        const byteBinary = binaryStr.slice(i*8, (i+1)*8);
-        byteArray[i] = parseInt(byteBinary, 2);
-    }
-
-    const textDecoder = new TextDecoder();   // go UTF-8
-    return textDecoder.decode(byteArray);
-}
-
-// test
-const textFromBinary = binaryToText(binaryRepresentation);
-console.log(textFromBinary);
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
     const encryptBtn = document.getElementById('encryptBtn');
@@ -41,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const inputText = document.getElementById('inputText');
     const encryptedTextOutput = document.getElementById('encryptedTextOutput');
 
-    const testKey = [0,0,1,1,0,0,0,0, 0,0,1,1,0,0,0,1, 0,0,1,1,0,0,1,0, 0,0,1,1,0,0,1,1, 0,0,1,1,0,1,0,0, 0,0,1,1,0,1,0,1, 0,0,1,1,0,1,1,0, 0,0,1,1,0,1,1,1];
+    let testKey = '133457799BBCDFF1';
 
     encryptBtn.addEventListener('click', function() {
         if (inputFile.files.length) {
@@ -49,13 +19,19 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             let textToEncrypt = inputText.value;
             if (textToEncrypt) {
-                textToEncrypt = textToBinary(textToEncrypt);  // go binary
-                const encryptedText = desEncrypt(textToEncrypt, testKey);
+                // Convert from UTF-8 to Hex
+                console.log("Text to encrypt: " + textToEncrypt)
+                let hexText = utf8ToHex(textToEncrypt);
+                console.log("Hex text: " + hexText);
+                // Pad using PKCS7
+                let paddedHexText = pkcs7Pad(hexText);
+                console.log("Padded hex text: " + paddedHexText);
+                const encryptedText = desEncrypt(paddedHexText, testKey);
                 encryptedTextOutput.value = encryptedText; // update text box
             } else {
                 console.warn('No text to encrypt.');
             }
-
         }
     });
+
 });
